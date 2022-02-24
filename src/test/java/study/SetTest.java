18e78726e9b8a4e91723d2618c7d9d246602ad3e
dunowljj <uuufg3322@gmail.com>
2022-02-24 23:16:10 +0900

    add learning-test-practice

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,53 @@
+package study;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    // Test Case 구현
+    @DisplayName("Set Size 테스트")
+    @Test
+    void size(){
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @DisplayName("Set contain 테스트")
+    @Test
+    void isContains(){
+        assertThat(numbers).contains(1);
+        assertThat(numbers).contains(2);
+        assertThat(numbers).contains(3);
+    }
+    @DisplayName("Set contain Parameterized 테스트")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void isContainsWithParameters(int number){
+        assertThat(numbers).contains(number);
+
+    }
+
+    @DisplayName("Set contain ParameterizedCSV 테스트")
+    @ParameterizedTest
+    @CsvSource({"1,true", "2,true", "3,true", "4,false", "5,false"})
+    void isContainsWithCSV(int number, boolean expected){
+        Assertions.assertEquals(numbers.contains(number), expected);
+    }
+}
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,10 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
@@ -10,4 +12,64 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @DisplayName("split 1,2")
+    @Test
+    void split12(){
+        String[] actual = "1,2".split(",");
+        assertThat(actual).contains("1");
+        assertThat(actual).contains("2");
+    }
+
+    @DisplayName("split 1")
+    @Test
+    void split1(){
+        String[] actual = "1".split(",");
+        assertThat(actual).containsExactly("1");
+    }
+
+    @Test
+    void substring(){
+        String actual = "(1,2)".substring(1,4);
+        assertThat(actual).isEqualTo("1,2");
+    }
+
+    @Nested
+    class charAt{
+
+        @Test
+        @DisplayName("기능테스트")
+        void charAt(){
+            char actual = "abc".charAt(0);
+            assertThat(actual).isEqualTo('a');
+
+            actual = "abc".charAt(1);
+            assertThat(actual).isEqualTo('b');
+
+            actual = "abc".charAt(2);
+            assertThat(actual).isEqualTo('c');
+        }
+
+        @Test
+        @DisplayName("예외테스트")
+        void charAtException(){
+            assertThatThrownBy(() -> "abc".charAt(3))
+                    .isInstanceOf(StringIndexOutOfBoundsException.class)
+                        .hasMessageContaining("String index out of range: 3");
+
+
+    //                .hasMessageContaining("String index out of range: 3");
+    //        assertThatThrownBy(() -> {throw new StringIndexOutOfBoundsException("boom");})
+    //                .isInstanceOf(StringIndexOutOfBoundsException.class)
+    //                .hasMessageContaining("boom");
+        }
+        @Test
+        @DisplayName("예외테스트2")
+        void charAtException2() {
+            assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                    .isThrownBy(() -> "abc".charAt(3))
+                    .withMessageMatching("String index out of range: \\d+");
+        }
+    }
+
 }
