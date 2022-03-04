deb6375c96871e4871e218f3b702407c6cb34a7e
dunowljj <uuufg3322@gmail.com>
2022-03-04 12:43:42 +0900

    modify DivideOperator

--- src/main/java/study2/operator/DivideOperator.java
+++ src/main/java/study2/operator/DivideOperator.java
@@ -4,6 +4,7 @@ public class DivideOperator implements Operator{
 
     @Override
     public int operate(int value, int nextValue) throws ArithmeticException{
-        return value / nextValue;
+        int result = (int)Math.round((double) value / nextValue); //소수 첫째자리에서 반올림한 값 리턴
+        return result;
     }
 }
